7e2b4bce8bc205feb8cd183e4437ed60c162c04d
krste <ksleona22@gmail.com>
2023-12-03 14:40:10 +0100

    av7.1 finished

--- av7/av7.1.cpp
+++ av7/av7.1.cpp
@@ -1,14 +1,40 @@
-//
-// Created by krste on 03/12/2023.
-//
+#include <iostream>
+using namespace std;
 
 int main(){
-    int n1, n2,element, i;
-    int a[100], b[100];
+    int n1, n2, element, i;
+    cin >> n1;
+    cin >> n2;
 
+    int a[100], b [100];
 
+    if (n1 != n2){
+        cout << "arrays are not equal" << endl;
+    }
+    else{
+        cout << "Elements of the first array:" << endl;
+        for (i = 0 ; i < n1 ; i++){
+            cout << "a[ " << i << "]=" << endl;
+            cin >> a [i];
+        }
+        cout << "Elements of the second array:"<< endl;
+        for (i = 0 ; i < n2 ; i++){
+            cout << "b[ " << i << "]= " << endl;
+            cin >> b[i];
+        }
+        for (i = 0; i < n1 ; i++){
+            if (a[i] != b[i]){
+                break;
+            }
+        }
+        if (i == n1){
+            cout << "arrays are equal" << endl;
 
-
+        }
+        else{
+            cout << "Arrays are not equal" << endl;
+        }
+    }
 
     return 0;
 }
