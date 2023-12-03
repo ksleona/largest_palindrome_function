08c4720b3b93c0f0aba16d96ebff3304a3fc23cd
krste <ksleona22@gmail.com>
2023-12-03 14:18:32 +0100

    Refactor Code

--- main.cpp
+++ main.cpp
@@ -2,7 +2,39 @@
 using namespace std;
 
 int main(){
-    int i;
-    
+    int n1, n2, element, i;
+    cin >> n1;
+    cin >> n2;
+
+    int a[100], b [100];
+
+    if (n1 != n2){
+        cout << "arrays are not equal" << endl;
+    }
+    else{
+        cout << "Elements of the first array:" << endl;
+        for (i = 0 ; i < n1 ; i++){
+            cout << "a[ " << i << "]=" << endl;
+            cin >> a [i];
+        }
+        cout << "Elements of the second array:"<< endl;
+        for (i = 0 ; i < n2 ; i++){
+            cout << "b[ " << i << "]= " << endl;
+            cin >> b[i];
+        }
+        for (i = 0; i < n1 ; i++){
+            if (a[i] != b[i]){
+                break;
+            }
+        }
+        if (i == n1){
+            cout << "arrays are equal" << endl;
+
+        }
+        else{
+            cout << "Arrays are not equal" << endl;
+        }
+    }
+
     return 0;
 }
---
+++ sturkturno_nizi.cpp
@@ -0,0 +1,14 @@
+//
+// Created by krste on 03/12/2023.
+//
+
+int main(){
+    int n1, n2,element, i;
+    int a[100], b[100];
+    c
+
+
+
+
+    return 0;
+}
