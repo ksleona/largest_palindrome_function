d20ef28bef03135259267f6880609951c36a3d79
krste <ksleona22@gmail.com>
2024-01-21 13:14:35 +0100

    Auditoriski zapochnati; vtora zavrshena

---
+++ AV/Av1/1.cpp
@@ -0,0 +1,13 @@
+//
+// Created by krste on 21/01/2024.
+//
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    char c;
+    cin >> c;
+    cout << char(c + ('a' - 'A')) << endl;
+     return 0;
+}
